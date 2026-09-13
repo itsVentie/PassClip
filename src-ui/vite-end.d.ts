f1ac1f220fe346b22472be4acff5d83ee4696694
itsVentie <ventieravelle@gmail.com>
2026-09-13 13:34:13 +0200

    fix(ui): update tsconfig include path to src/ui and add vite env declaration

--- src-ui/vite-end.d.ts
+++ src-ui/vite-end.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
+/// <reference types="preact" />
 
+declare module '*.css';
 declare module '*.module.css' {
   const classes: { readonly [key: string]: string };
   export default classes;
 }
-
-declare module '*.css';
