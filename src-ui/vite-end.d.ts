305942ea452ee1a3052cb02d0f90f202a10ddcca
itsVentie <ventieravelle@gmail.com>
2026-09-13 13:32:02 +0200

    fix(ui): configure preact jsx compiler options and vite env declarations

--- src-ui/vite-end.d.ts
+++ src-ui/vite-end.d.ts
@@ -1,6 +1,8 @@
 /// <reference types="vite/client" />
 
 declare module '*.module.css' {
-  const classes: { [key: string]: string };
+  const classes: { readonly [key: string]: string };
   export default classes;
 }
+
+declare module '*.css';
