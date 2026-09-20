1f289d0e784eb3f0a7c9d04e69a51fffc408139d
itsVentie <ventieravelle@gmail.com>
2026-09-20 09:31:01 +0200

    logs added to the tray

--- src-tauri/src/tray.rs
+++ src-tauri/src/tray.rs
@@ -1,3 +1,4 @@
+use crate::logs;
 use tauri::{
     menu::{Menu, MenuItem},
     tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
@@ -19,8 +20,14 @@ pub fn setup_tray(app: &AppHandle) -> Result<(), Box<dyn std::error::Error>> {
         .menu(&menu)
         .show_menu_on_left_click(false)
         .on_menu_event(|app, event| match event.id.as_ref() {
-            "toggle" => toggle_window(app),
-            "quit" => app.exit(0),
+            "toggle" => {
+                logs::log(app, "DEBUG", "Tray menu item clicked: Toggle Window");
+                toggle_window(app);
+            }
+            "quit" => {
+                logs::log(app, "INFO", "Tray menu item clicked: Quit PassClip");
+                app.exit(0);
+            }
             _ => {}
         })
         .on_tray_icon_event(|tray, event| {
@@ -30,11 +37,13 @@ pub fn setup_tray(app: &AppHandle) -> Result<(), Box<dyn std::error::Error>> {
                 ..
             } = event
             {
+                logs::log(tray.app_handle(), "DEBUG", "Tray icon left clicked");
                 toggle_window(tray.app_handle());
             }
         })
         .build(app)?;
 
+    logs::log(app, "INFO", "System tray icon initialized");
     Ok(())
 }
 
@@ -42,10 +51,14 @@ fn toggle_window(app: &AppHandle) {
     if let Some(window) = app.get_webview_window("main") {
         let is_visible = window.is_visible().unwrap_or(false);
         if is_visible {
+            logs::log(app, "DEBUG", "Hiding main window to tray");
             let _ = window.hide();
         } else {
+            logs::log(app, "DEBUG", "Showing and focusing main window");
             let _ = window.show();
             let _ = window.set_focus();
         }
+    } else {
+        logs::log(app, "ERROR", "Main window instance not found when toggling");
     }
 }
